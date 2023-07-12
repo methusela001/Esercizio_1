a8c706ba3db8d1106d667e0bdacba7b679a40b73
Methusela <nethusela777@gmail.com>
2023-07-12 15:47:45 +0200

    strampa

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,12 @@
+package co.develhope.introduction;
+
+public class Start {
+        //psvm
+        public static void main(String[] args) {
+            //sout
+            System.out.println("Hello develhope java student");
+            System.out.println("02/05/2000");
+        }
+
+    }
+
